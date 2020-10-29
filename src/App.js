da384f54b94bf803c01c6fbf2670ebbcaf35069f
LoganRothrock <loganrothrock@outlook.com>
2020-10-29 16:51:01 -0500

    Completed features 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {ToDoBanner} from './ToDoBanner';
+import 'bootstrap/dist/css/bootstrap.css';
 
+export default class App extends Component {
+  //  Above we have created a class called App the extends the functionality of the Component class
+
+  //  The export keyword above makes the class available for use outside of the JS file where it is created
+
+  constructor() {
+
+    super();
+
+    //  React components have a special property called "state".  The "state" is used to define the state of data (props)
+
+    this.state = {
+      userName: "Shawn Nelson",
+      todoItems: [
+        {action: "Move burn pile", done: false},
+        {action: "Sweep Garage", done: true},
+        {action: "Mow", done: false},
+        {action: "Get Corolla from shop", done: false},
+        {action: "Ask daughter for more Chai", done: false}
+      ]
+    }
+
+  } // END CONSTRUCTOR
+
+  render = () =>
+    <div id="startingPoint">
+      {/* Features 1 & 2 */}
+      <ToDoBanner   
+        userName = {this.state.userName}
+        todoItems = {this.state.todoItems}
+      />
+    </div>
+
+}
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +62,6 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
+//export default App;
---
+++ src/ToDoBanner.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export class ToDoBanner extends Component {
+    //  Features 1 & 2
+    render = () =>
+        <h4 className="bg-primary text-white text-center p-2">
+            {this.props.userName}'s To Do List -- {this.props.todoItems.filter(x => !x.done).length} items still unfinsihed
+        </h4>
+};
